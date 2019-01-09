7a4aa6e83fe7150caf30021e5a605d62f2919fe0
unknown <15901453613@163.com>
2019-01-09 16:31:12 +0800

    icon

--- src/views/home/components/Icons.vue
+++ src/views/home/components/Icons.vue
@@ -1,98 +1,35 @@
 <template>
-  <div class="icons">
-    <!-- arrow="never" -->
-    <Carousel v-model="value2" loop  :radius-dot="true">
+  <div class="icons swiper">
+    <!-- arrow="never" dots="outside" -->
+    <!-- iviwe实现的 -->
+    <!-- <Carousel v-model="value2" dots="outside"  :radius-dot="true">
       <CarouselItem v-for="(page,index) in pages" :key="index">
       <div class="demo-carousel">
         <div class="icon" v-for="item in page" :key="item.id">
           <div class="icon-img">
-            <img class="icon-img-content" :src="item.imgUrl" alt>
+            <img class="icon-img-content" :src="item.imgUrl" :alt="item.desc">
           </div>
           <p class="icon-desc">{{item.desc}}</p>
         </div>
       </div>
       </CarouselItem>
-      <!-- <CarouselItem>
-        <div class="demo-carousel">2</div>
-      </CarouselItem>
-      <CarouselItem>
-        <div class="demo-carousel">3</div>
-      </CarouselItem>
-      <CarouselItem>
-        <div class="demo-carousel">4</div>
-      </CarouselItem>-->
-    </Carousel>
-
+    </Carousel>-->
     <!-- <div class="icon">
-      <div class="icon-img">
-        <img
-          class="icon-img-content"
-          src="http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png"
-          alt
-        >
-      </div>
-      <p class="icon-desc">一日游</p>
+      <div class="icon-img"></div>
     </div>
-    <div class="icon">
-      <div class="icon-img">
-        <img
-          class="icon-img-content"
-          src="http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png"
-          alt
-        >
-      </div>
-      <p class="icon-desc">北京必游</p>
-    </div>
-    <div class="icon">
-      <div class="icon-img">
-        <img
-          class="icon-img-content"
-          src="http://img1.qunarzz.com/piao/fusion/1803/20/831d62d2e1c7be02.png"
-          alt
-        >
-      </div>
-      <p class="icon-desc">帝都范儿</p>
-    </div>
-    <div class="icon">
-      <div class="icon-img">
-        <img
-          class="icon-img-content"
-          src="http://img1.qunarzz.com/piao/fusion/1803/76/eb88861d78fb9902.png"
-          alt
-        >
-      </div>
-      <p class="icon-desc">植物园儿</p>
-    </div>
-    <div class="icon">
-      <div class="icon-img">
-        <img
-          class="icon-img-content"
-          src="http://img1.qunarzz.com/piao/fusion/1803/6c/9e54a8540fee0102.png"
-          alt
-        >
-      </div>
-      <p class="icon-desc">故宫</p>
-    </div>
-    <div class="icon">
-      <div class="icon-img">
-        <img
-          class="icon-img-content"
-          src="http://img1.qunarzz.com/piao/fusion/1803/20/831d62d2e1c7be02.png"
-          alt
-        >
-      </div>
-      <p class="icon-desc">名胜古迹</p>
-    </div>
-    <div class="icon">
-      <div class="icon-img">
-        <img
-          class="icon-img-content"
-          src="http://img1.qunarzz.com/piao/fusion/1803/fc/b10a6b2e4f0fe102.png"
-          alt
-        >
-      </div>
-      <p class="icon-desc">北京滑雪</p>
-    </div>-->
+    <p class="icon-desc"></p>-->
+    <!-- 适应手机端 -->
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="(page,index) in pages" :key="index">
+        <div class="icon" v-for="item in page" :key="item.id">
+          <div class="icon-img">
+            <img class="icon-img-content" :src="item.imgUrl" :alt="item.desc">
+          </div>
+          <p class="icon-desc">{{item.desc}}</p>
+        </div>
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
   </div>
 </template>
 <script>
@@ -111,52 +48,59 @@ export default {
         {
           id: "0002",
           imgUrl:
-            "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
-          desc: "景点门票"
+            "http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png",
+          desc: "一日游"
         },
         {
           id: "0003",
           imgUrl:
-            "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
-          desc: "景点门票"
+            "http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png",
+          desc: "北京必游"
         },
-         {
+        {
           id: "0004",
           imgUrl:
-            "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
-          desc: "景点门票"
+            "http://img1.qunarzz.com/piao/fusion/1803/20/831d62d2e1c7be02.png",
+          desc: "帝都范儿"
         },
-         {
+        {
           id: "0005",
           imgUrl:
-            "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
-          desc: "景点门票"
+            "http://img1.qunarzz.com/piao/fusion/1803/76/eb88861d78fb9902.png",
+          desc: "动植物园"
         },
-         {
+        {
           id: "0006",
           imgUrl:
-            "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
-          desc: "景点门票"
+            "http://img1.qunarzz.com/piao/fusion/1803/6c/9e54a8540fee0102.png",
+          desc: "故宫"
         },
-         {
+        {
           id: "0007",
           imgUrl:
-            "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
-          desc: "景点门票"
+            "http://img1.qunarzz.com/piao/fusion/1803/20/831d62d2e1c7be02.png",
+          desc: "名胜古迹"
         },
-         {
+        {
           id: "0008",
           imgUrl:
-            "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
-          desc: "景点门票"
+            "http://img1.qunarzz.com/piao/fusion/1803/fc/b10a6b2e4f0fe102.png",
+          desc: "北京滑雪"
         },
-         {
+        {
           id: "0009",
           imgUrl:
-            "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
-          desc: "景点门票"
+            "http://img1.qunarzz.com/piao/fusion/1803/ab/6f7d6e44963c9302.png",
+          desc: "泡温泉"
+        },
+        {
+          id: "0010",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1803/b2/bfbc5d66e0ab5a02.png",
+          desc: "冰雪大世界"
         }
-      ]
+      ],
+      swiperOption: {}
     };
   },
   computed: {
@@ -168,7 +112,7 @@ export default {
           pages[page] = [];
         }
         pages[page].push(iterm);
-      })
+      });
       return pages;
     }
   }
@@ -176,11 +120,14 @@ export default {
 </script>
 <style lang="less" scoped>
 @import "../../../assets/styles/varibles.less";
+// .icons /deep/ .ivu-carousel-active{
+//   color: #ffffff;
+// }
 .icons {
   overflow: hidden;
   height: 0;
-  padding-bottom: 50%;
-    // bottom: green;
+  padding-bottom: 55%;
+  // bottom: green;
   .icon {
     position: relative;
     overflow: hidden;
@@ -195,7 +142,7 @@ export default {
       left: 0;
       right: 0;
       bottom: 0.44rem;
-        // background-color: blue;
+      // background-color: blue;
       box-sizing: border-box;
       padding: 0.2rem;
       .icon-img-content {
