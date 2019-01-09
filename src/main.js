e6d9ac941cb647eaa8ad9d8c643b16e4026a5ca5
unknown <15901453613@163.com>
2019-01-09 17:38:51 +0800

    add swiper

--- src/main.js
+++ src/main.js
@@ -3,11 +3,14 @@ import App from './App.vue'
 import './plugins/iview.js'
 import router from './router'
 import fastClick from "fastclick"
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import "./assets/styles/reset.css"
 import "./assets/styles/border.css"
+import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
 
 new Vue({
   router,
--- src/views/home/components/HomeSwiper.vue
+++ src/views/home/components/HomeSwiper.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="swiper">
-    <Carousel v-model="value1" :radius-dot="true">
+    <!-- 使用移动端 -->
+    <!-- <Carousel v-model="value1" :radius-dot="true">
       <CarouselItem>
         <div class="demo-carousel">
           <img
@@ -33,29 +34,48 @@
           >
         </div>
       </CarouselItem>
-    </Carousel>
+    </Carousel>-->
+    <!-- 使用手机端 -->
+    <swiper :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide>
+        <div class="demo-carousel">
+          <img src="http://img1.qunarzz.com/piao/fusion/1808/70/5603e6a535062402.jpg_750x200_2c552f2c.jpg" alt="最美山水">
+        </div>
+      </swiper-slide>
+      <swiper-slide>
+          <div class="demo-carousel">
+          <img src="http://img1.qunarzz.com/piao/fusion/1901/17/d1821879b04ca602.jpg_750x200_115ccc5d.jpg" alt="冰雪嘉年华">
+        </div>
+      </swiper-slide>
+      <swiper-slide>
+          <div class="demo-carousel">
+          <img src="http://img1.qunarzz.com/piao/fusion/1812/d6/daa880b254940402.jpg_750x200_b114308a.jpg" alt="年终狂欢">
+        </div>
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
   </div>
 </template>
 <script>
 export default {
   data() {
     return {
-      value1: 0
+      value1: 0,
+      swiperOption: {}
     };
-  },
-  name: "HomeSwiper"
+  }
 };
 </script>
 <style lang="less" scoped>
-.swiper /deep/ .ivu-carousel-dots li button {
-  background-color: #fff !important;
-}
+// .swiper  .ivu-carousel-dots li button {
+//   background-color: #fff !important;
+// }
 .swiper {
-  overflow: hidden;
   width: 100%;
   height: 0;
   padding-bottom: 31.25%;
-  background-color: #eee;
   img {
     width: 100%;
     height: 100%;
