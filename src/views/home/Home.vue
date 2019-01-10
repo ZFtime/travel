ef84bd74dd9f97a48e17c4fba382a552896e5a4f
unknown <15901453613@163.com>
2019-01-10 11:45:21 +0800

    add commend

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,20 +3,26 @@
     <HomeHeader/>
     <HomeSwiper/>
     <HomeIcons/>
+    <HomeRecommend/>
+    <HomeWeekend/>
   </div>
 </template>
 
 <script>
 import HomeHeader from "./components/Header";
 import HomeSwiper from "./components/HomeSwiper";
-import HomeIcons  from "./components/Icons"
+import HomeIcons from "./components/Icons";
+import HomeRecommend from "./components/Recommend";
+import HomeWeekend from "./components/Weekend";
 
 export default {
   name: "home",
   components: {
     HomeHeader,
     HomeSwiper,
-    HomeIcons
+    HomeIcons,
+    HomeRecommend,
+    HomeWeekend
   }
 };
 </script>
---
+++ src/views/home/components/Weekend.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="weekend">
+    <div class="title">周末去哪儿</div>
+    <ul>
+      <li class="item border-bottom" v-for="item in WeekendList" :key="item.id">
+        <div class="item-img-wrapper">
+          <img class="item-img" :src="item.imgUrl" :alt="item.itemTitle">
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{item.itemTitle}}</p>
+          <p class="item-desc">{{item.itemDesc}}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: "HomeWeekend",
+  data() {
+    return {
+      WeekendList: [
+        {
+          id: "0001",
+          imgUrl:
+            "http://img1.qunarzz.com/sight/p0/1712/95/95f38f28a6ff19cba3.img.jpg_200x200_af967e35.jpg",
+          itemTitle: "水奥雪世界",
+          itemDesc: "北京市顺义区奥林匹克水上公园"
+        },
+        {
+          id: "0002",
+          imgUrl:
+            "http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_200x200_1bc99086.jpg",
+          itemTitle: "故宫",
+          itemDesc: "北京市东城区景山前街4号"
+        },
+        {
+          id: "0003",
+          imgUrl:
+            "http://img1.qunarzz.com/sight/p0/1511/d2/d2aec2dfc5aa771290.water.jpg_200x200_69e4a80d.jpg",
+          itemTitle: "南山滑雪场",
+          itemDesc: "北京市密云县河南寨镇圣水头村"
+        }
+      ]
+    };
+  }
+};
+</script>
+<style lang="less" scoped>
+.weekend {
+  background-color: #fff;
+  .title {
+    line-height: 0.8rem;
+    background: #eee;
+    text-indent: 0.2rem;
+  }
+  .item-img-wrapper {
+    overflow: hidden;
+    height: 0;
+    padding-bottom: 33.9%;
+    .item-img {
+      width: 100%;
+    }
+  }
+
+  .item-info {
+    flex: 1;
+    padding: 0.1rem;
+    .item-title {
+      line-height: 0.54rem;
+      font-size: 0.32rem;
+    }
+    .item-desc {
+      line-height: 0.4rem;
+      color: #ccc;
+    }
+  }
+}
+</style>
+
+
