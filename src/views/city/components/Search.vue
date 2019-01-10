8073f8c4f078c292ee6dd8632c4e2b8fedf111c6
unknown <15901453613@163.com>
2019-01-10 22:28:43 +0800

    add search to city

---
+++ src/views/city/components/Search.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="search">
+      <input class="search-input" type="text" placeholder="输入城市名或拼音">
+  </div>
+</template>
+<script>
+export default {
+  name: "CitySearch"
+};
+</script>
+<style lang="less" scoped>
+@import "../../../assets/styles/varibles.less";
+.search{
+    height: .82rem;
+    padding: .1rem;
+    background-color: @bgColor;
+    .search-input{
+        box-sizing: border-box;
+        padding: .15rem;
+        height: .62rem;
+        width: 100%;
+        text-align: center;
+        border-radius: .06rem;
+        color: #666 !important;
+    }
+}
+</style>
