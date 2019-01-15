ad52c6be8ed4d322de7aac3bdd18050e33cb74b9
unknown <15901453613@163.com>
2019-01-15 16:45:26 +0800

    detailList

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -2,22 +2,53 @@
   <div class="detail">
     <Banner/>
     <DetailHeader/>
+    <DetailList :list="list"/>
     <div class="content"></div>
   </div>
 </template>
 <script>
 import Banner from "./components/Banner";
 import DetailHeader from "./components/Header";
+import DetailList from "./components/List";
 export default {
   name: "Detail",
   components: {
     Banner,
-    DetailHeader
+    DetailHeader,
+    DetailList
+  },
+  data() {
+    return {
+      list: [
+        {
+          title: "成人票",
+          children: [
+            {
+              title: "成人票三级联票",
+              children: [
+                {
+                  title: "成人票三级联票————112"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          title: "学生票"
+        },
+        {
+          title: "儿童票"
+        },
+        {
+          title: "特惠票"
+        }
+      ]
+    };
   }
 };
 </script>
 <style lang="less" scoped>
-.content{
-    height: 50rem;
+.content {
+  height: 50rem;
 }
 </style>
---
+++ src/views/detail/components/List.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <div class="item" v-for="(item,index) in list" :key="index">
+      <div class="item-title border-bottom">
+        <span class="item-title-icon"></span>
+        {{item.title}}
+      </div>
+      <div v-if="item.children" class="item-children">
+          <DetailList :list="item.children"/>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "DetailList",
+  data() {
+    return {};
+  },
+  props: {
+    list: Array
+  },
+  methods: {}
+};
+</script>
+<style lang="less" scoped>
+@import "../../../assets/styles/varibles.less";
+.item-title-icon {
+  display: inline-block;
+  position: relative;
+  top: .08rem;
+  left: .06rem;
+  width: 0.36rem;
+  height: 0.36rem;
+  //   top: 0.26rem;
+  //   left: 0.2rem;
+  background: url(http://s.qunarzz.com/piao/image/touch/sight/detail.png) 0 -0.45rem
+    no-repeat;
+  margin-right: 0.1rem;
+  background-size: 0.4rem 3rem;
+}
+.item-title {
+  line-height: 0.8rem;
+  font-size: 0.32rem;
+  padding: 0 0.2rem;
+}
+.item-children{
+    padding: 0 .2rem;
+}
+</style>
