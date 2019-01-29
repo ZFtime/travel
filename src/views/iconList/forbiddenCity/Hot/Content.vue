23006038f104b2a84dbcd108d0fdb95a5a716fe5
unknown <15901453613@163.com>
2019-01-29 14:36:21 +0800

    forbidden-hot  optimization

--- src/views/iconList/forbiddenCity/Hot/Content.vue
+++ src/views/iconList/forbiddenCity/Hot/Content.vue
@@ -12,85 +12,19 @@
     <!-- 玩转故宫 -->
     <div class="cont" v-show="show==0">
       <ul class="detail-more clearfix">
-        <li class="item-more">
+        <li class="item-more" v-for="(item,index) in playList" :key="index">
           <div class="item-cont">
             <div class="item-img">
-              <img
-                src="https://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_290x180_aad89f75.jpg"
-                alt="故宫"
-              >
-              <span class="name">故宫</span>
+              <img :src="item.bannenImg" :alt="item.sighttName">
+              <span class="name">{{item.sighttName}}</span>
             </div>
             <div class="info">
-              <h3 class="title">【下午场】故宫成人票凭身份证快速入园（淡季）</h3>
+              <h3 class="title">{{item.sighttInfoTitle}}</h3>
               <div class="lure">
                 <span class="hot-btn">抢购</span>
                 <span class="hot-price">
                   ¥
-                  <em class="hot-price-num">40</em>
-                </span>
-              </div>
-            </div>
-          </div>
-        </li>
-        <li class="item-more">
-          <div class="item-cont">
-            <div class="item-img">
-              <img
-                src="https://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_290x180_aad89f75.jpg"
-                alt="故宫"
-              >
-              <span class="name">故宫</span>
-            </div>
-            <div class="info">
-              <h3 class="title">【下午场】故宫成人票凭身份证快速入园（淡季）</h3>
-              <div class="lure">
-                <span class="hot-btn">抢购</span>
-                <span class="hot-price">
-                  ¥
-                  <em class="hot-price-num">40</em>
-                </span>
-              </div>
-            </div>
-          </div>
-        </li>
-        <li class="item-more">
-          <div class="item-cont">
-            <div class="item-img">
-              <img
-                src="https://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_290x180_aad89f75.jpg"
-                alt="故宫"
-              >
-              <span class="name">故宫</span>
-            </div>
-            <div class="info">
-              <h3 class="title">【下午场】故宫成人票凭身份证快速入园（淡季）</h3>
-              <div class="lure">
-                <span class="hot-btn">抢购</span>
-                <span class="hot-price">
-                  ¥
-                  <em class="hot-price-num">40</em>
-                </span>
-              </div>
-            </div>
-          </div>
-        </li>
-        <li class="item-more">
-          <div class="item-cont">
-            <div class="item-img">
-              <img
-                src="https://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_290x180_aad89f75.jpg"
-                alt="故宫"
-              >
-              <span class="name">故宫</span>
-            </div>
-            <div class="info">
-              <h3 class="title">【下午场】故宫成人票凭身份证快速入园（淡季）</h3>
-              <div class="lure">
-                <span class="hot-btn">抢购</span>
-                <span class="hot-price">
-                  ¥
-                  <em class="hot-price-num">40</em>
+                  <em class="hot-price-num">{{item.InfoPrice}}</em>
                 </span>
               </div>
             </div>
@@ -101,85 +35,19 @@
     <!-- 热销套票 -->
     <div class="cont" v-show="show==1">
       <ul class="detail-more clearfix">
-        <li class="item-more">
+        <li class="item-more" v-for="(item,index) in SellWellList" :key="index">
           <div class="item-cont">
             <div class="item-img">
-              <img
-                src="https://img1.qunarzz.com/piao/fusion/1607/41/5837c598ef45b8b6.png_290x180_d5ab7067.png"
-                alt="故宫"
-              >
-              <span class="name">故宫</span>
+              <img :src="item.bannenImg" :alt="item.sighttName">
+              <span class="name">{{item.sighttName}}</span>
             </div>
             <div class="info">
-              <h3 class="title">【下午场】故宫成人票凭身份证快速入园（淡季）</h3>
+              <h3 class="title">{{item.sighttInfoTitle}}</h3>
               <div class="lure">
                 <span class="hot-btn">抢购</span>
                 <span class="hot-price">
                   ¥
-                  <em class="hot-price-num">40</em>
-                </span>
-              </div>
-            </div>
-          </div>
-        </li>
-        <li class="item-more">
-          <div class="item-cont">
-            <div class="item-img">
-              <img
-                src="https://img1.qunarzz.com/piao/fusion/1607/41/5837c598ef45b8b6.png_290x180_d5ab7067.png"
-                alt="故宫"
-              >
-              <span class="name">故宫</span>
-            </div>
-            <div class="info">
-              <h3 class="title">【下午场】故宫成人票凭身份证快速入园（淡季）</h3>
-              <div class="lure">
-                <span class="hot-btn">抢购</span>
-                <span class="hot-price">
-                  ¥
-                  <em class="hot-price-num">40</em>
-                </span>
-              </div>
-            </div>
-          </div>
-        </li>
-        <li class="item-more">
-          <div class="item-cont">
-            <div class="item-img">
-              <img
-                src="https://img1.qunarzz.com/piao/fusion/1607/41/5837c598ef45b8b6.png_290x180_d5ab7067.png"
-                alt="故宫"
-              >
-              <span class="name">故宫</span>
-            </div>
-            <div class="info">
-              <h3 class="title">【下午场】故宫成人票凭身份证快速入园（淡季）</h3>
-              <div class="lure">
-                <span class="hot-btn">抢购</span>
-                <span class="hot-price">
-                  ¥
-                  <em class="hot-price-num">40</em>
-                </span>
-              </div>
-            </div>
-          </div>
-        </li>
-        <li class="item-more">
-          <div class="item-cont">
-            <div class="item-img">
-              <img
-                src="https://img1.qunarzz.com/piao/fusion/1607/41/5837c598ef45b8b6.png_290x180_d5ab7067.png"
-                alt="故宫"
-              >
-              <span class="name">故宫</span>
-            </div>
-            <div class="info">
-              <h3 class="title">【下午场】故宫成人票凭身份证快速入园（淡季）</h3>
-              <div class="lure">
-                <span class="hot-btn">抢购</span>
-                <span class="hot-price">
-                  ¥
-                  <em class="hot-price-num">40</em>
+                  <em class="hot-price-num">{{item.InfoPrice}}</em>
                 </span>
               </div>
             </div>
@@ -190,85 +58,19 @@
     <!-- 周边必游 -->
     <div class="cont" v-show="show==2">
       <ul class="detail-more clearfix">
-        <li class="item-more">
+        <li class="item-more" v-for="(item,index) in MustTourList" :key="index">
           <div class="item-cont">
             <div class="item-img">
-              <img
-                src="https:////img1.qunarzz.com/sight/p0/201405/12/51b63883c25f5d87af3c08bb016e2bd7.jpg_290x180_b88ccfe6.jpg"
-                alt="恭王府"
-              >
-              <span class="name">恭王府</span>
+              <img :src="item.bannenImg" :alt="item.sighttName">
+              <span class="name">{{item.sighttName}}</span>
             </div>
             <div class="info">
-              <h3 class="title">北京恭王府门票+人工讲解服务*成人票</h3>
+              <h3 class="title">{{item.sighttInfoTitle}}</h3>
               <div class="lure">
                 <span class="hot-btn">抢购</span>
                 <span class="hot-price">
                   ¥
-                  <em class="hot-price-num">60</em>
-                </span>
-              </div>
-            </div>
-          </div>
-        </li>
-        <li class="item-more">
-          <div class="item-cont">
-            <div class="item-img">
-              <img
-                src="https:////img1.qunarzz.com/sight/p0/201405/12/51b63883c25f5d87af3c08bb016e2bd7.jpg_290x180_b88ccfe6.jpg"
-                alt="恭王府"
-              >
-              <span class="name">恭王府</span>
-            </div>
-            <div class="info">
-              <h3 class="title">北京恭王府门票+人工讲解服务*成人票</h3>
-              <div class="lure">
-                <span class="hot-btn">抢购</span>
-                <span class="hot-price">
-                  ¥
-                  <em class="hot-price-num">60</em>
-                </span>
-              </div>
-            </div>
-          </div>
-        </li>
-        <li class="item-more">
-          <div class="item-cont">
-            <div class="item-img">
-              <img
-                src="https:////img1.qunarzz.com/sight/p0/201405/12/51b63883c25f5d87af3c08bb016e2bd7.jpg_290x180_b88ccfe6.jpg"
-                alt="恭王府"
-              >
-              <span class="name">恭王府</span>
-            </div>
-            <div class="info">
-              <h3 class="title">北京恭王府门票+人工讲解服务*成人票</h3>
-              <div class="lure">
-                <span class="hot-btn">抢购</span>
-                <span class="hot-price">
-                  ¥
-                  <em class="hot-price-num">60</em>
-                </span>
-              </div>
-            </div>
-          </div>
-        </li>
-        <li class="item-more">
-          <div class="item-cont">
-            <div class="item-img">
-              <img
-                src="https:////img1.qunarzz.com/sight/p0/201405/12/51b63883c25f5d87af3c08bb016e2bd7.jpg_290x180_b88ccfe6.jpg"
-                alt="恭王府"
-              >
-              <span class="name">恭王府</span>
-            </div>
-            <div class="info">
-              <h3 class="title">北京恭王府门票+人工讲解服务*成人票</h3>
-              <div class="lure">
-                <span class="hot-btn">抢购</span>
-                <span class="hot-price">
-                  ¥
-                  <em class="hot-price-num">60</em>
+                  <em class="hot-price-num">{{item.InfoPrice}}</em>
                 </span>
               </div>
             </div>
@@ -284,7 +86,105 @@ export default {
   data() {
     return {
       show: 0,
-      List: ["玩转故宫", "热销套票", "周边必游"]
+      List: ["玩转故宫", "热销套票", "周边必游"],
+      playList: [
+        {
+          sighttName: "故宫",
+          sighttInfoTitle: "【下午场】故宫成人票凭身份证快速入园（淡季）",
+          sightInfoDetail: "东方宫殿建筑代表，世界宫殿建筑典范",
+          InfoPrice: "40",
+          bannenImg:
+            "https://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_290x180_aad89f75.jpg"
+        },
+        {
+          sighttName: "故宫",
+          sighttInfoTitle: "【下午场】故宫成人票凭身份证快速入园（淡季）",
+          sightInfoDetail: "东方宫殿建筑代表，世界宫殿建筑典范",
+          InfoPrice: "40",
+          bannenImg:
+            "https://img1.qunarzz.com/piao/fusion/1607/41/5837c598ef45b8b6.png_290x180_d5ab7067.png"
+        },
+        {
+          sighttName: "故宫",
+          sighttInfoTitle: "【下午场】故宫成人票凭身份证快速入园（淡季）",
+          sightInfoDetail: "东方宫殿建筑代表，世界宫殿建筑典范",
+          InfoPrice: "40",
+          bannenImg:
+            "https://img1.qunarzz.com/piao/fusion/1605/fc/e31ec51c58632ff7.jpg_290x180_a0b9afd2.jpg"
+        },
+        {
+          sighttName: "故宫",
+          sighttInfoTitle: "【下午场】故宫成人票凭身份证快速入园（淡季）",
+          sightInfoDetail: "东方宫殿建筑代表，世界宫殿建筑典范",
+          InfoPrice: "40",
+          bannenImg:
+            "https://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_290x180_aad89f75.jpg"
+        }
+      ],
+      SellWellList: [
+        {
+          sighttName: "故宫",
+          sighttInfoTitle: "【下午场】故宫成人票凭身份证快速入园（淡季）",
+          sightInfoDetail: "东方宫殿建筑代表，世界宫殿建筑典范",
+          InfoPrice: "40",
+          bannenImg:
+            "https://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_290x180_aad89f75.jpg"
+        },
+        {
+          sighttName: "故宫",
+          sighttInfoTitle: "【下午场】故宫成人票凭身份证快速入园（淡季）",
+          sightInfoDetail: "东方宫殿建筑代表，世界宫殿建筑典范",
+          InfoPrice: "40",
+          bannenImg:
+            "https://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_290x180_aad89f75.jpg"
+        },
+        {
+          sighttName: "故宫",
+          sighttInfoTitle: "【下午场】故宫成人票凭身份证快速入园（淡季）",
+          sightInfoDetail: "东方宫殿建筑代表，世界宫殿建筑典范",
+          InfoPrice: "40",
+          bannenImg:
+            "https://img1.qunarzz.com/piao/fusion/1607/56/7e27875077bb51b6.jpg_290x180_fa225081.jpg"
+        },
+        {
+          sighttName: "故宫",
+          sighttInfoTitle: "【下午场】故宫成人票凭身份证快速入园（淡季）",
+          sightInfoDetail: "东方宫殿建筑代表，世界宫殿建筑典范",
+          InfoPrice: "40",
+          bannenImg:
+            "https://img1.qunarzz.com/piao/fusion/1607/41/5837c598ef45b8b6.png_290x180_d5ab7067.png"
+        }
+      ],
+      MustTourList: [
+        {
+          sighttName: "颐和园",
+          sighttInfoTitle: "北京颐和园门票+资深导游讲解服务·成人票",
+          InfoPrice: "40",
+          bannenImg:
+            "https://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_290x180_aad89f75.jpg"
+        },
+        {
+          sighttName: "恭王府",
+          sighttInfoTitle: "北京恭王府门票+人工讲解服务*成人票",
+          InfoPrice: "60",
+          bannenImg:
+            "https://img1.qunarzz.com/piao/fusion/1607/41/5837c598ef45b8b6.png_290x180_d5ab7067.png"
+        },
+        {
+          sighttName: "什刹海",
+          sighttInfoTitle: "北京北海公园门票成人票",
+          InfoPrice: "5",
+          bannenImg:
+            "https://img1.qunarzz.com/sight/p0/1603/85/85c883189bf7293390.water.jpg_290x180_fca0a68d.jpg"
+        },
+        {
+          sighttName: "水立方",
+          sighttInfoTitle: "水立方成人票",
+          InfoPrice: "40",
+          bannenImg:
+            "https://img1.qunarzz.com/sight/p0/1501/40/40b2b6c951b28fdd.water.jpg_290x180_9d3bf220.jpg"
+        }
+      ]
     };
   },
   methods: {
